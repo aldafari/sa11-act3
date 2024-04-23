a9dd85f67fc4c16a6c0781fb9c7b9e1d5b8893ef
aldafari <aldafari@memphis.edu>
2024-04-23 18:32:47 -0500

    Solved problem 1.

--- prob1.rb
+++ prob1.rb
@@ -1,6 +1,8 @@
 def extract_urls(text)
+  pattern = /https?:\/\/[\S]+/
+  url = text.scan(pattern)
+  url.flatten
 end
 
 sample_text = "Visit our site at http://www.example.org for more information. Check out our search page at https://example.com/search?q=ruby+regex. Don’t forget to ftp our resources at ftp://example.com/resources."
-
-extract_urls(sample_text)
+p extract_urls(sample_text)
